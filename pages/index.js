43035febf1bfba3adf18194f3a35d0eb7fe5bd1d
Aaron Panchal <aaron.panchal@gmail.com>
2021-08-10 22:09:09 -0700

    Load content for course index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
-export default function Home() {
+import { getCourses } from "../lib/api";
+
+export default function Home({ courses }) {
   return (
     <div>
       <Head>
@@ -15,24 +17,30 @@ export default function Home() {
       </nav>
 
       <main className="p-7 sm:px-14 max-w-4xl">
-        <div className="flex gap-x-7 text-sm sm:text-base">
-          <div>
-            <p className="font-mono text-lg sm:text-xl">CS-130</p>
-            <p className="text-gray-500 font-normal">Fall 2021</p>
+        {courses.map(({ code, term, title, excerpt }) => (
+          <div
+            key={`${code}_${term}`}
+            className="flex gap-x-7 text-sm sm:text-base"
+          >
+            <div>
+              <p className="font-mono text-lg sm:text-xl">{code}</p>
+              <p className="text-gray-500 font-normal">{term}</p>
+            </div>
+            <div className="flex-1 flex flex-col gap-y-2">
+              <h2 className="font-bold font-display">{title}</h2>
+              <p className="leading-relaxed">{excerpt}</p>
+            </div>
           </div>
-          <p className="flex-1 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            ultricies nulla nec elit venenatis, vel consequat nibh blandit.
-            Praesent hendrerit consequat faucibus. Mauris vitae mattis nisi.
-            Suspendisse potenti. Fusce rhoncus quis nisi id rhoncus. Nam
-            pharetra iaculis fringilla. Aenean iaculis mauris leo, eu placerat
-            nibh rutrum non. Nullam in lacus vel ex semper fringilla eleifend
-            vel eros. Suspendisse feugiat erat est. Nunc lectus metus, facilisis
-            non turpis et, tincidunt mollis leo. Vestibulum aliquet neque ut
-            suscipit vehicula.
-          </p>
-        </div>
+        ))}
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const courses = await getCourses();
+
+  return {
+    props: { courses },
+  };
 }
