0fce09132fb124f7077585101a9c059b3c26c8ff
Aaron Panchal <aaron.panchal@gmail.com>
2021-08-10 22:07:47 -0700

    Fix crossOrigin attribute in document

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,11 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://use.typekit.net" />
           <link rel="preconnect" href="https://p.typekit.net" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,300;0,400;0,700;1,200;1,400;1,700&family=JetBrains+Mono:wght@200;400;700&display=swap"
