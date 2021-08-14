fae16fc3caf3f403ac30ebb7c2aa1474eb12a0c3
Aaron Panchal <aaron.panchal@gmail.com>
2021-08-13 19:21:23 -0700

    Clean up mobile/larger styles for course list

--- pages/index.js
+++ pages/index.js
@@ -23,18 +23,18 @@ export default function Home({ courses }) {
       <main className="p-7 sm:py-9 max-w-xl mx-auto">
         {courses.map(({ code, term, title, excerpt, courseId }) => (
           <Link href={`/${courseId}`} key={`${code}_${term}`}>
-            <a>
-              <div className="flex flex-col sm:flex-row gap-y-4 gap-x-7 text-sm sm:text-base p-4 -m-4 sm:p-7 sm:-m-7 hover:bg-gray-100 rounded-lg">
+            <a className="flex flex-col gap-y-3 sm:gap-y-4text-sm sm:text-base hover:bg-gray-100 rounded-lg p-4 -m-4 sm:p-7 sm:-m-7">
+              <div className="flex items-center gap-x-5 sm:gap-x-7">
                 <div>
                   <p className="font-mono text-lg sm:text-xl">{code}</p>
                   <p className="text-gray-500 font-normal">{term}</p>
                 </div>
                 <div className="flex-1 flex flex-col gap-y-2">
                   <h2 className="font-bold font-display">{title}</h2>
-                  <div className="prose prose-sm">
-                    <ReactMarkdown>{excerpt}</ReactMarkdown>
-                  </div>
                 </div>
+              </div>
+              <div className="prose prose-sm sm:ml-7">
+                <ReactMarkdown>{excerpt}</ReactMarkdown>
               </div>
             </a>
           </Link>
