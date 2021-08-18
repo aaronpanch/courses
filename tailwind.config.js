5fa039954697200bb51c55806ed3135d3169475b
Aaron Panchal <aaron.panchal@gmail.com>
2021-08-17 20:36:48 -0700

    Clean up inline code theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
       display: ["futura-pt-bold"],
       body: ["Futura PT Sans", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: theme("colors.grey.100"),
+              color: "#DD1144",
+              fontWeight: "400",
+              "border-radius": "0.25rem",
+            },
+            "code::before": {
+              content: '""',
+              "padding-left": "0.25rem",
+            },
+            "code::after": {
+              content: '""',
+              "padding-right": "0.25rem",
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
